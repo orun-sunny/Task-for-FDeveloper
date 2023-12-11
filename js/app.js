363cd666e0ad29c080b7047e1fbc8918e57a5bfb
orun sunny <sunny.mukut@gmail.com>
2023-12-11 17:44:22 +0600

    close modal

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@
     const form = document.getElementById("uploadForm");
     let targetCard;
 
+    // this will Open modal
+    attachmentfileBtn.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            targetCard = e.target.parentElement.parentElement.querySelector('.attachmentup-btn span');
+            modal.classList.add('show');
+        });
+    })
+
+    // Close modal 
+    modal.addEventListener('click', e => {
+        if (e.target.classList.contains('modal')) {
+            modal.classList.remove('show');
+        }
+    })
+
 
 
 })();
