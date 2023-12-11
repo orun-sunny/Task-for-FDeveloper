537486a3223dc4bb025daec3a31009395f6c2e1a
orun sunny <sunny.mukut@gmail.com>
2023-12-12 00:21:48 +0600

    added app.js

--- js/app.js
+++ js/app.js
@@ -20,6 +20,38 @@
         }
     })
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const attachments = form.attachments.files;
+        const formData = new FormData();
+        // Append each selected file to the FormData object
+        for (let i = 0; i < attachments.length; i++) {
+            formData.append('attachments', attachments[i]);
+        }
 
+        // Fetch API
+        fetch('/api/v1/attachment-upload', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    targetCard.innerText = targetCard.innerText * 1 + data.filenames.length;
+                    const fileList = document.getElementById("fileList");
+                    fileList.innerHTML = ''
+                    data.filenames.forEach(function (filename) {
+                        const listItem = document.createElement("li");
+                        listItem.textContent = filename;
+                        fileList.appendChild(listItem);
+                    });
+                } else {
+                    alert("File upload failed: " + data.message);
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    })
 
 })();
