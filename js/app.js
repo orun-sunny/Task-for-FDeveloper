ce1df1d3f272a90e38a3b4ed67ebbe3007912916
orun sunny <sunny.mukut@gmail.com>
2023-12-11 17:28:19 +0600

    added modal

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Show modal and upload attachment
+(function () {
+    const attachmentfileBtn = document.querySelectorAll('.attachmentup-btn');
+    const modal = document.querySelector('.modal');
+    const form = document.getElementById("uploadForm");
+    let targetCard;
+
+
+
+})();
